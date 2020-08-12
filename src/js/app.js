21c520f6473c5969baf8aa3e6cf0075524d23ac2
Bazarov Maxim <bazarow.max@gmail.com>
2020-08-13 01:21:27 +0800

    added header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
-import hello from './hello'
-console.log('app js')
-hello()
+const burgerButton = document.getElementById('burgerButton')
+const mobileMenu = document.getElementById('mobileMenu')
+const body = document.querySelector('body')
+
+burgerButton.addEventListener('click', ()=>{
+    mobileMenu.classList.toggle('header__nav_active')
+    body.classList.toggle('body_active')
+})
