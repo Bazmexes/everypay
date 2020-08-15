af90268da4cc56fbc02ed2215e3a5766c5fc3316
Bazarov Maxim <bazarow.max@gmail.com>
2020-08-15 08:25:54 +0800

    95%

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,41 @@ burgerButton.addEventListener('click', ()=>{
     mobileMenu.classList.toggle('header__nav_active')
     body.classList.toggle('body_active')
 })
+// var swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 'auto',
+//     spaceBetween: 30,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+let width = window.innerWidth
+console.log(width)
+var between;
+if(width<375){
+    between=230
+}else if(width<420){
+    between=180
+}else if(width<500){
+    between=150
+}else if(width<550){
+    between=100
+}else if(width<600){
+    between=30
+}
+else if(width>700){
+    between=0
+}
+console.log(between)
+var slides;
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    centeredSlides: true,
+    spaceBetween: between,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
